45cb57ddbfadfa163de7b11df82e6802dbd9fcb3
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-08-10 11:16:58 +0000

    Release 0.0.3

--- src/Client.ts
+++ src/Client.ts
@@ -51,8 +51,8 @@ export class SonykClient {
                 {
                     "X-Fern-Language": "JavaScript",
                     "X-Fern-SDK-Name": "sonyk-sdk",
-                    "X-Fern-SDK-Version": "0.0.2",
-                    "User-Agent": "sonyk-sdk/0.0.2",
+                    "X-Fern-SDK-Version": "0.0.3",
+                    "User-Agent": "sonyk-sdk/0.0.3",
                     "X-Fern-Runtime": core.RUNTIME.type,
                     "X-Fern-Runtime-Version": core.RUNTIME.version,
                 },
